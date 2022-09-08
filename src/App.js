de95a5e04a0eb5f33bcb736e712812f053142b45
Murtaza Pervez <murtazapervez615@gmail.com>
2022-09-08 17:10:08 +0500

    Added router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import { useState, useEffect } from "react";
+import { FaTasks } from "react-icons/fa";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import AddTaskForm from "./components/AddTaskForm";
 import Header from "./components/Header";
 import TaskList from "./components/TaskList";
 
 
 function App() {
-  
+
   const [tasks, setTasks] = useState([
     {
-      "id":1,
-      "title":"New Task",
-      "description":"Lorem Ipsum is simply dummy text of the printing and type setting industry.",
-      "completed":false,
-      "date_time":"15 September, 2022"
+      "id": 1,
+      "title": "New Task",
+      "description": "Lorem Ipsum is simply dummy text of the printing and type setting industry.",
+      "completed": false,
+      "date_time": "15 September, 2022"
     }
   ])
 
@@ -20,30 +22,39 @@ function App() {
     setTasks([...tasks, task])
   }
 
-  const markCompleted = async(id) => {
+  const markCompleted = async (id) => {
     setTasks(
-      tasks.map((task) => task.id === id ? { ...task, completed: !task.completed } : task )
+      tasks.map((task) => task.id === id ? { ...task, completed: !task.completed } : task)
     )
   }
 
   const [showForm, setShowForm] = useState(false)
 
-  const toggleForm = async() => {
+  const toggleForm = async () => {
     setShowForm(!showForm)
   }
 
-  const removeTask = async(id) => {
+  const removeTask = async (id) => {
     setTasks(
-        tasks.filter((task) => task.id !== id )
-      )
+      tasks.filter((task) => task.id !== id)
+    )
   }
-  
+
   return (
-    <div className="container">
-      <Header title='Add Tasks' onClick={toggleForm}/>
-      {showForm && <AddTaskForm onAdd={addTask}/> } 
-      <TaskList tasks={tasks} onDoubleClick={markCompleted} onDelete={removeTask}/>
-    </div>
+      <Router>
+        <div className="box">
+            <div>
+              <Link to="/"><FaTasks/> Home</Link>
+            </div>
+            <div>
+              <Link to="/about">About</Link>
+            </div>
+            <div>
+              <Link to="/dashboard">Dashboard</Link>
+            </div>
+        </div>
+        </Router>
+     
   );
 }
 
---
+++ src/pages/projects/AddProject.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AddProject() {
+  return (
+    <h1>Add Project</h1>
+  )
+}
